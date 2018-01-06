e36d462fa7fba69c6f16a12a8bb2b774c0bc0227
Dennis Mok <dennismok@MacBook-Air.local>
2018-01-05 22:24:28 -0800

    Revert "added skelton for Login and Sign Up screens"
    This reverts commit 546079397e99aca364ad4daeb54bbdfe94fc7356.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import Router from './Router';
 import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
 
 
 export default class App extends Component<{}> {
   render() {
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
-        <Router />
+        <View>
+          <Text>
+            Hello
+          </Text>
+        </View>
       </Provider>
     );
   }
